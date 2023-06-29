5b3c67dd8bfed8bf710a5e7e05e99620f98d41e1
AceHunterr <aggarwalmehul26@gmail.com>
2023-06-30 04:50:16 +0530

    Init commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { render } from 'preact'
+import App from './app'
+import './index.css'
+
+render(<App />, document.getElementById('app'))
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// import App from "./app";
+// import "./index.css";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
